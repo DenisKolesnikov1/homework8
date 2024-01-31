6784ed1b3fed1bc279279fd914dd2e751263d931
Денис Колесников <73371887@mail.ru>
2024-01-31 21:03:05 +0300

    homework8

--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,19 @@ public class Main {
         massiv[0] = 1;
         massiv[1] = 2;
         massiv[2] = 3;
-
         System.out.println(Arrays.toString(massiv));
 
-        double[] deimals = {1.57, 7.654, 9.986};
-
-        String[] namesOfAnimals = {"TigerCub", "Calf", "Wolf"};
-        int[] ageOfTheAnimals = {10, 20, 15};
-
-        double[] weightOfTheBeast = new double[3];
-        weightOfTheBeast[0] = 100.0;
-        weightOfTheBeast[1] = 150.0;
-        weightOfTheBeast[2] = 200.0;
-
-        System.out.println(Arrays.toString(weightOfTheBeast));
-
+        double[] tiger = {1.57, 7.654, 9.986};
+        for (int i = tiger.length - 1; i >= 0 ; i--) {
+            System.out.print(tiger[i]);
+            if (i != 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
 
         //
-        int[] ints =new int[]{-1, -2, -3, -4, -5, -6};
+        int[] ints ={1, 2, 3, 4, 5, 6};
         for (int i = 0; i < ints.length; i++) {
             System.out.print( ints[i]);
             if (i != ints.length - 1 ) {
@@ -33,23 +28,28 @@ public class Main {
             }
         }
         System.out.println();
-        for (int i = ints.length-1; i >= 0; i--) {
-            System.out.print(ints[i]);
+
+        //
+        int[] ints1 ={1, 2, 3, 4, 5, 6};
+        for (int i = ints1.length -1; i >= 0; i--) {
+            System.out.print(ints1[i]);
             if (i != 0) {
                 System.out.print(", ");
             }
         }
+        System.out.println();
+        double[] dog = {1.57, 7.654, 9.986};
+        for (int i = 0; i < dog.length; i++) {
+            System.out.print(dog[i]);
+            if (i != ints.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
 
         //
-        {
-            System.out.println();
-            for (int i = 0; i < ints.length; i++) {
-                if (ints[i] % 2 != 0) {
-                    ints[i] = ints[i] + 1;
-                }
-            }
-            System.out.println(Arrays.toString(ints));
+        int[] massiv1 = {1, 2, 3};
+        System.out.println(Arrays.toString(massiv1));
 
-        }
     }
 }

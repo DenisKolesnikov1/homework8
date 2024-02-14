99f4469d15aaf50bd95f34b52c90b04859f11445
Денис Колесников <73371887@mail.ru>
2024-02-14 21:32:23 +0300

    homework8.3

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        //1.
-        int[] massiv;
-        massiv = new int[]{1, 2, 3};
-        massiv[0] = 1;
-        massiv[1] = 2;
-        massiv[2] = 3;
-        System.out.println(Arrays.toString(massiv));
-
-        double[] tiger = {1.57, 7.654, 9.986};
-        for (int i = tiger.length - 1; i >= 0 ; i--) {
-            System.out.print(tiger[i]);
-            if (i != 0) {
-                System.out.print(",");
-            }
-        }
-        System.out.println();
-
-        //2.
-        int[] ints ={1, 2, 3, 4, 5, 6};
-        for (int i = 0; i < ints.length; i++) {
-            System.out.print( ints[i]);
-            if (i != ints.length - 1 ) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-
-        //3.
-        int[] ints1 ={1, 2, 3, 4, 5, 6};
-        for (int i = ints1.length -1; i >= 0; i--) {
-            System.out.print(ints1[i]);
-            if (i != 0) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-        double[] dog = {1.57, 7.654, 9.986};
-        for (int i = 0; i < dog.length; i++) {
-            System.out.print(dog[i]);
-            if (i != ints.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.println();
-
-        //4.
+        //1
         int[] massiv1 = {1, 2, 3};
         System.out.println(Arrays.toString(massiv1));
 
@@ -65,6 +20,51 @@ public class Main {
                 System.out.print(", ");
             }
         }
+
+        //2
+        int[] massiv;
+        massiv = new int[]{1, 2, 3};
+        massiv[0] = 1;
+        massiv[1] = 2;
+        massiv[2] = 3;
+        System.out.println(Arrays.toString(massiv));
+
+        double[] tiger = {1.57, 7.654, 9.986};
+        for (int i = tiger.length - 1; i >= 0 ; i--) {
+            System.out.print(tiger[i]);
+            if (i != 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+
+        //3
+        int[] ints ={1, 2, 3, 4, 5, 6};
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print( ints[i]);
+            if (i != ints.length - 1 ) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        //4
+        int[] ints1 ={1, 2, 3, 4, 5, 6};
+        for (int i = ints1.length -1; i >= 0; i--) {
+            System.out.print(ints1[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        double[] dog = {1.57, 7.654, 9.986};
+        for (int i = 0; i < dog.length; i++) {
+            System.out.print(dog[i]);
+            if (i != ints.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
 //
     }
 }
